725daf5ebd77b58f45e01b62a4961b9882e24c95
Ghosty366 <32891973+Ghosty366@users.noreply.github.com>
2020-07-18 10:24:36 -0500

    Clean up some code

--- main/java/com/ghosty366/morestuffmod/util/RegistryHandler.java
+++ main/java/com/ghosty366/morestuffmod/util/RegistryHandler.java
@@ -29,14 +29,15 @@ public class RegistryHandler {
     public static final RegistryObject<Item> STEEL_PLATING = ITEMS.register("steel_plating", ItemBase::new);
     public static final RegistryObject<Cheeze> CHEEZE = ITEMS.register("cheeze", Cheeze::new);
     public static final RegistryObject<Item> STEEL = ITEMS.register("steel", ItemBase::new);
+
     // Tools
-    public static final RegistryObject<SwordItem> STEEL_SWORD = ITEMS.register("steel_sword", () ->
-            new SwordItem(ModItemTier.STEEL, 0, -2.4F, new Item.Properties().group(MoreStuff.TAB)));
-    public static final RegistryObject<PickaxeItem> STEEL_PICKAXE = ITEMS.register("steel_pickaxe", () ->
-            new PickaxeItem(ModItemTier.STEEL, -4, -2.8F, new Item.Properties().group(MoreStuff.TAB)));
+    public static final RegistryObject<SwordItem> STEEL_SWORD = ITEMS.register("steel_sword", () -> new SwordItem(ModItemTier.STEEL, 0, -2.4F, new Item.Properties().group(MoreStuff.TAB)));
+    public static final RegistryObject<PickaxeItem> STEEL_PICKAXE = ITEMS.register("steel_pickaxe", () -> new PickaxeItem(ModItemTier.STEEL, -4, -2.8F, new Item.Properties().group(MoreStuff.TAB)));
+
     // Blocks
     public static final RegistryObject<Block> STEEL_BLOCK = BLOCKS.register("steel_block", SteelBlock::new);
     public static final RegistryObject<Block> STEEL_ORE = BLOCKS.register("steel_ore", SteelOre::new);
+
     // Block Items
     public static final RegistryObject<Item> STEEL_BLOCK_ITEM = ITEMS.register("steel_block", () -> new BlockItemBase(STEEL_BLOCK.get()));
     public static final RegistryObject<Item> STEEL_ORE_ITEM = ITEMS.register("steel_ore", () -> new BlockItemBase(STEEL_ORE.get()));
