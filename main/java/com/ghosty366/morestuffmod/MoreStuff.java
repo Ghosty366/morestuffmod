a0a7c3b54694671215b810fc0b4737078eafa827
Ghosty366 <32891973+Ghosty366@users.noreply.github.com>
2020-08-02 14:34:20 -0500

    Added Husky to the mod with some bugs that i will fix

--- main/java/com/ghosty366/morestuffmod/MoreStuff.java
+++ main/java/com/ghosty366/morestuffmod/MoreStuff.java
@@ -1,9 +1,14 @@
 package com.ghosty366.morestuffmod;
 
+import com.ghosty366.morestuffmod.entities.HuskyEntity;
+import com.ghosty366.morestuffmod.init.ModEntityTypes;
 import com.ghosty366.morestuffmod.util.RegistryHandler;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -20,14 +25,17 @@ public class MoreStuff {
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
+        ModEntityTypes.ENTITY_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
         RegistryHandler.init();
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) { }
-
+    private void setup(final FMLCommonSetupEvent event) {
+        DeferredWorkQueue.runLater(() -> {
+            GlobalEntityTypeAttributes.put(ModEntityTypes.HUSKY.get(), HuskyEntity.setCustomAttributes().create());
+        });
+    }
     private void doClientStuff(final FMLClientSetupEvent event) { }
 
     public static final ItemGroup TAB = new ItemGroup("morestuffTab") {
